7659869d557e1dabd4b5a4ae48d00b84fdd98a3c
Dylan Santiago <notfennecks@gmail.com>
2021-12-02 21:22:01 -0500

    Layout of New Map

---
+++ UnityProject/SpaceTradersAndRaiders/Assets/UniverseMapManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UniverseMapManager : MonoBehaviour
+{
+    private void OnMouseEnter()
+    {
+        Debug.Log(this.name);
+    }
+}
